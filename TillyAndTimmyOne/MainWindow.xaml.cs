b1c62c5d69ab89bf09801a38cca392a319aba6d9
Mike O'Neill <mike.oneill@blueyonder.co.uk>
2018-06-13 21:06:30 +0100

    Add project files.

---
+++ TillyAndTimmyOne/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TillyAndTimmyOne
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void playerNameTextbox_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                doYouWannaPlayTextblock.Visibility = Visibility.Visible;
+                chooseGameButton.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void chooseGameButton_Click(object sender, RoutedEventArgs e)
+        {
+            ChooseGameWindow ChooseGameWindow = new ChooseGameWindow();
+            ChooseGameWindow.Visibility = Visibility.Visible;
+            this.Visibility = Visibility.Hidden;
+        }
+    }
+}
